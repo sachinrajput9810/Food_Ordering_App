665b7d7b27bcc1888eb7d816777bf2a985500c59
Sachin Rajput <165654923+sachinrajput9810@users.noreply.github.com>
2024-09-07 22:07:53 +0530

    update Body component and style and removed mockdata

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,24 @@
 import RestaurantCard  from "./RestaurantCard"
-import resList from "../utils/mockData"
-import {useState} from "react"
+import {useState , useEffect} from "react"
+import Shimmer from "./Shimmer"
 
 let Body = () => {
 
-    const [listOfRestaurant , setListOfRestaurant] = useState(resList)
+    const [listOfRestaurant , setListOfRestaurant] = useState([])
+    
+    useEffect(()=> {
+        fetchData() ;
+    } , [])
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.66500&lng=77.44770&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json() ;
+        console.log(json)
+        setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+
+    // conditional rendering
+    if(listOfRestaurant.length === 0) return <Shimmer/>
 
     return (
         <div className="body">
